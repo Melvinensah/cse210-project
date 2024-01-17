ecab8110c0bf4d1f479a4053487f94967128e708
Melvinensah <melvinbunduka@gmail.com>
2024-01-17 04:50:01 +0000

    abstraction learning activity completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using Learning02;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Alafia";
+        job1._jobTitle = "Sales Agent";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        // job1.DisplayJob();
+
+        Job job2 = new Job();
+        job2._company = "Kings Krown";
+        job2._jobTitle = "Customer Service";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // job2.DisplayJob();
+
+        Resume resume = new Resume();
+        resume.name = "Melvinensah Bunduka";
+        resume.jobs.Add(job1);
+        resume.jobs.Add(job2);
+        resume.DisplayResume();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+namespace Learning02
+{
+    public class Resume
+    {
+        public string name;
+        public List<Job> jobs = new List<Job>();
+        public Resume()
+        {}
+        public void DisplayResume()
+        {
+            Console.WriteLine(name);
+            foreach(Job job in jobs)
+            {
+                job.DisplayJob();
+            }
+        }
+    }
+}
